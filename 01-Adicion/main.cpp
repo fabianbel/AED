ff91c06113cae04543e0913138e2ad55ec2578c0
fabianbel <38517063+fabianbel@users.noreply.github.com>
2019-05-01 22:24:54 -0300

    Update main.cpp

--- 01-Adicion/main.cpp
+++ 01-Adicion/main.cpp
@@ -1,13 +1,17 @@
+/* Adicion
+Fabian Belmonte
+20190425
+*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-int A,B,SUM;
+int A,B;
     cout << "Ingrese un numero entero" << endl;
     cin >> A;
     cout << "Ingrese un numero entero" << endl;
     cin >> B;
-    SUM=A+B;
-    cout << "La suma es: " << SUM << endl;
+    cout << "La suma es: " << A+B << endl;
     }
