f0323fff031fe743781215830f773572aee2809f
Fabián <fabianbel@yahoo.com.ar>
2019-05-01 21:49:43 -0300

    00-CHelloWorld

---
+++ 00-CHelloWorld/Hello.cpp
@@ -0,0 +1,8 @@
+/* Hello World
+Fabian Belmonte
+20190410
+*/
+#include <iostream>
+int main() {
+std::cout << "Hello, World!\n";
+}
---
+++ 00-CHelloWorld/README.md
@@ -0,0 +1,4 @@
+***Algoritmos y Estuctura de Datos*** 
+Curso: K1051
+ 2019 
+Trabajo Practico: 00-Hello World
